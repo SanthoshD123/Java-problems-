867037bed2f191609de1c4a1d24b7e28cdfe9816
Santhosh D <141542516+SanthoshD123@users.noreply.github.com>
2023-08-12 22:29:42 +0530

    Create factorial .java

---
+++ factorial .java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int n = 5;
+	    int i = 1;
+	    while(n>0){
+	        i = i*n;
+	        n--;
+	    }
+	    System.out.print(i);
+	}
+}
